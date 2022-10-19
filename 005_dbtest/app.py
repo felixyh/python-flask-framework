78db74bef5a0aae33b720fe8d42aaf3a964e9287
“felixyh” <felixyanghui@gmail.com>
2022-10-19 21:25:40 +0800

    update

--- 005_dbtest/app.py
+++ 005_dbtest/app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 import config
 
 
 app = Flask(__name__)
-db = flask_sqlalchemy(app)
+db = SQLAlchemy(app)
 
 app.config.from_object(config)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
